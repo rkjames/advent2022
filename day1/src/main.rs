f3061b52f5a7910e75b657860e14d2d8edbde6de
Rick James <rkjames@hotmail.com>
2022-11-30 22:43:22 -0800

    day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let data = fs::read_to_string("src\\test1.txt").expect("read to string failed");
     let lines: Vec<&str> = data.split("\n").collect();
     println!("{}", lines.len());
-    let mut elf: Vec<i32> = Vec::new();
+    let mut elf: Vec<(i32, i32)> = Vec::new();
     let mut current = 0;
-    elf.push(0);
+    elf.push((0, 0));
     for line in &lines {
         if line.len() == 1 {
-            elf.push(0);
+            elf.push((0, 0));
             current += 1;
         } else {
             let guess: i32 = match line.trim().parse() {
@@ -18,17 +18,14 @@ fn main() {
                 Err(_) => continue,
             };
 
-            elf[current] += guess;
+            elf[current].1 += guess;
         }
     }
     println!("{:?}", elf);
-    let mut max_cal = 0;
-    let mut max_index = 0;
-    for (pos, e) in elf.iter().enumerate() {
-        if *e > max_cal {
-            max_cal = *e;
-            max_index = pos;
-        }
-    }
-    println!("ask elf {}, carrying {}", max_index + 1, elf[max_index]);
+    elf.sort_by_key(|k| k.1);
+    let i = elf.len() - 1;
+    println!(
+        "top 3 elves carrying {}",
+        elf[i].1 + elf[i - 1].1 + elf[i - 2].1
+    );
 }
