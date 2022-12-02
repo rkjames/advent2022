4aca4a73dfafe288a9ffd3b38bc2cd190918ca37
Rick James <rkjames@hotmail.com>
2022-12-01 22:25:01 -0800

    switch to enum

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,76 +1,80 @@
 use std::fs;
 
+#[derive(Debug, PartialEq, Copy, Clone)] 
+enum Move {
+    Rock=1,
+    Paper=2,
+    Scissors=3
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)] 
+enum Goal {
+    Lose,
+    Draw,
+    Win
+}
+
 fn failfast(msg :&str) {
     println!("failfast {msg}");
     std::process::abort();
 }
 
-// todo: learn enums
-fn me_score(opp : i32, me : i32) -> i32 {
+fn me_score(opp : Move, me : Move) -> i32 {
     if opp == me {
         return 3;
     }
-    if opp == 1 && me == 2 {
-        // rock, paper
+    if opp == Move::Rock && me == Move::Paper {
         return 6;
     }
-    if opp == 2 && me == 3 {
-        // paper, scissors
+    if opp == Move::Paper && me == Move::Scissors {
         return 6;
     }
-    if opp == 3 && me == 1 {
-        // scissors, rock
+    if opp == Move::Scissors && me == Move::Rock {
         return 6;
     }
     0
 }
 
-fn me_choice(opp : i32, me : i32) -> i32 {
-    if me == 2 {
-        // draw
+fn me_choice(opp : Move, me : Goal) -> Move {
+    if me == Goal::Draw {
         return opp;
     }
 
-    if me == 1 {
-        // lose
+    if me == Goal::Lose {
         match opp {
-            1 => return 3,
-            2 => return 1,
-            3 => return 2, 
-            _ => failfast("me_choice lose") 
+            Move::Rock => return Move::Scissors,
+            Move::Paper => return Move::Rock,
+            Move::Scissors => return Move::Paper,
         }
     }
 
     // win
     match opp {
-        1 => return 2,
-        2 => return 3,
-        3 => return 1,     
-        // todo: why can't i failfast here?       
-        _ => 999999
+        Move::Rock => return Move::Paper,
+        Move::Paper => return Move::Scissors,
+        Move::Scissors => return Move::Rock,
     }
-
 }
 
 fn main() {
     println!("Hello, world!");
-    let data = fs::read_to_string("test1.txt").expect("read to string failed");
+    let data = fs::read_to_string("example1.txt").expect("read to string failed");
     let lines: Vec<&str> = data.split("\n").collect();
-    let mut opp : Vec<i32> = Vec::new();
-    let mut me : Vec<i32> = Vec::new();
+    let mut opp : Vec<Move> = Vec::new();
+    let mut me : Vec<Goal> = Vec::new();
     println!("{}", lines.len());
     for line in &lines {
         let s : Vec<&str> = line.trim().split(" ").collect();
         match s[0] {
-            "A" => opp.push(1),
-            "B" => opp.push(2),
-            "C" => opp.push(3),
+            "A" => opp.push(Move::Rock),
+            "B" => opp.push(Move::Paper),
+            "C" => opp.push(Move::Scissors),
             _ => failfast("opponent")
         }
         match s[1] {
-            "X" => me.push(1),
-            "Y" => me.push(2),
-            "Z" => me.push(3),
+            "X" => me.push(Goal::Lose),
+            "Y" => me.push(Goal::Draw),
+            "Z" => me.push(Goal::Win),
             _ => failfast("me")
         }
     }
@@ -78,12 +82,9 @@ fn main() {
     let mut me_total = 0;
     for i in 0..opp.len() {
         let choose = me_choice(opp[i], me[i]);
-        let round = me_score(opp[i], choose) + choose;
-        println!("{round}");
+        let round = me_score(opp[i], choose) + (choose as i32);
         me_total += round;
     }
 
-    //println!("{:?}", opp);
-    //println!("{:?}", me);
     println!("{me_total}");
 }
