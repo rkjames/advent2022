60f1f434e746cf3f8b9daa76d5854ea1d64e7b09
Rick James <rkjames@hotmail.com>
2022-12-03 21:19:23 -0800

    day 4 part 1

---
+++ day4/src/main.rs
@@ -0,0 +1,28 @@
+use std::{fs, collections::HashSet};
+
+fn main() {
+    println!("Hello, world!");
+    let data = fs::read_to_string("example1.txt").expect("read to string failed");
+    let lines: Vec<&str> = data.split("\n").collect();
+    let mut count = 0;
+    for line in &lines {
+        let section : Vec<&str> = line.trim().split(",").collect();
+        let first = range_to_set(section[0]);
+        let second = range_to_set(section[1]);
+        if first.is_subset(&second) || second.is_subset(&first) {
+            count +=1;
+        }
+    }
+    println!("{count} overlaps");
+}
+
+fn range_to_set(range: &str) -> HashSet<i32> {
+    let first :Vec<&str> = range.split("-").collect();
+    let first_start : i32 = first[0].parse().unwrap();
+    let first_end : i32 = first[1].parse().unwrap();
+    let mut first_set : HashSet<i32> = HashSet::new();
+    for i in first_start..first_end+1 {
+        first_set.insert(i);
+    }
+    first_set
+}
