552212d9c0f060e0d75027124d84852e81f13236
Rick James <rkjames@hotmail.com>
2022-12-08 22:18:00 -0800

    day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -13,46 +13,60 @@ fn parse() -> Vec<(char, i32)> {
     v
 }
 
-fn isclose(tx: i32, ty: i32, hx: i32, hy: i32) -> bool {
-    hx >= tx - 1 && hx <= tx + 1 && hy >= ty - 1 && hy <= ty + 1
+#[derive(Hash, Debug, Eq, PartialEq, Clone, Copy)]
+struct Knot {
+    x: i32,
+    y: i32,
 }
 
 fn main() {
     let v = parse();
     println!("{:?}", v);
 
-    let mut hx = 0;
-    let mut hy = 0;
-    let mut tx = 0;
-    let mut ty = 0;
-    let mut visit: HashSet<(i32, i32)> = HashSet::new();
+    let mut rope: Vec<Knot> = Vec::new();
+    for _i in 0..10 {
+        rope.push(Knot { x: 0, y: 0 });
+    }
+    let mut visit: HashSet<Knot> = HashSet::new();
 
     for (dir, val) in v {
         println!("{dir} {val}");
         for _i in 0..val {
-            match dir {
-                'R' => hx += 1,
-                'L' => hx -= 1,
-                'U' => hy += 1,
-                'D' => hy -= 1,
-                _ => unreachable!(),
-            }
-
-            if !isclose(tx, ty, hx, hy) {
-                if hy > ty {
-                    ty += 1
-                } else if hy < ty {
-                    ty -= 1
-                }
-                if hx > tx {
-                    tx += 1
-                } else if hx < tx {
-                    tx -= 1
+            {
+                let head = &mut rope[0];
+                match dir {
+                    'R' => head.x += 1,
+                    'L' => head.x -= 1,
+                    'U' => head.y += 1,
+                    'D' => head.y -= 1,
+                    _ => unreachable!(),
                 }
             }
 
-            println!("t({tx}, {ty}) -> h({hx}, {hy})");
-            visit.insert((tx, ty));
+            // todo: error[E0499]: cannot borrow `rope` as mutable more than once at a time
+            // --> no references --> hard to read
+            for i in 1..rope.len() {
+                if !(rope[i - 1].x >= rope[i].x - 1
+                    && rope[i - 1].x <= rope[i].x + 1
+                    && rope[i - 1].y >= rope[i].y - 1
+                    && rope[i - 1].y <= rope[i].y + 1)
+                {
+                    if rope[i - 1].y > rope[i].y {
+                        rope[i].y += 1
+                    } else if rope[i - 1].y < rope[i].y {
+                        rope[i].y -= 1
+                    }
+                    if rope[i - 1].x > rope[i].x {
+                        rope[i].x += 1
+                    } else if rope[i - 1].x < rope[i].x {
+                        rope[i].x -= 1
+                    }
+                }
+            }
+
+            //println!("{:?}", rope);
+            let tail = rope[rope.len() - 1];
+            visit.insert(tail);
         }
     }
 
