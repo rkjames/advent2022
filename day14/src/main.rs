c296e6484f44ca21e6a78e2ebb9d4119cf1a788a
Rick James <rkjames@hotmail.com>
2022-12-16 19:06:25 -0800

    day 14 part 1

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+#[derive(PartialEq)]
 enum Space {
     Air,
     Rock,
@@ -7,11 +8,11 @@ enum Space {
 }
 
 fn parse() -> Vec<Vec<Space>> {
-    let data = fs::read_to_string("example1.txt").expect("read to string failed");
+    let data = fs::read_to_string("test1.txt").expect("read to string failed");
     let lines: Vec<&str> = data.split("\n").collect();
     let mut v: Vec<Vec<Space>> = Vec::new();
     for _x in 0..600 {
-        let mut row : Vec<Space> = Vec::new();
+        let mut row: Vec<Space> = Vec::new();
         for _y in 0..200 {
             row.push(Space::Air);
         }
@@ -27,14 +28,19 @@ fn parse() -> Vec<Vec<Space>> {
             path.push((x, y));
         }
         for i in 1..path.len() {
-            let (x1,y1) = path[i-1];
-            let (x2,y2) = path[i];
+            let (x1, y1) = path[i - 1];
+            let (x2, y2) = path[i];
+            //println!("path {x1},{y1}->{x2},{y2}");
             if x1 == x2 {
-                for y in y1..=y2 {
+                let miny = std::cmp::min(y1, y2);
+                let maxy = std::cmp::max(y1, y2);
+                for y in miny..=maxy {
                     v[x1][y] = Space::Rock;
                 }
             } else {
-                for x in x1..=x2 {
+                let minx = std::cmp::min(x1, x2);
+                let maxx = std::cmp::max(x1, x2);
+                for x in minx..=maxx {
                     v[x][y1] = Space::Rock;
                 }
             }
@@ -42,7 +48,80 @@ fn parse() -> Vec<Vec<Space>> {
     }
     v
 }
+
+fn print(v: &Vec<Vec<Space>>) {
+    for y in 0..50 {
+        for x in 450..550 {
+            if x == 500 && y == 0 {
+                print!("+");
+            } else {
+                let c = match v[x][y] {
+                    Space::Air => '.',
+                    Space::Rock => '#',
+                    Space::Sand => 'O',
+                };
+                print!("{c}");
+            }
+        }
+        println!("");
+    }
+}
 fn main() {
-    let map = parse();
-    println!("{}", map.len());
+    let mut map = parse();
+    //print(&map);
+
+    // example should take 24 iterations
+    'grains: loop {
+        // The sand is pouring into the cave from point 500,0.
+        let mut x = 500;
+        let mut y = 0;
+
+        loop {
+            if y >= 199 {
+                // off the bottom
+                //println!("break bottom");
+                break 'grains;
+            }
+
+            if map[x][y + 1] == Space::Air {
+                // A unit of sand always falls down one step if possible.
+                y += 1;
+                continue;
+            }
+            if map[x - 1][y + 1] == Space::Air {
+                // the unit of sand attempts to instead move diagonally one step down and to the left.
+                y += 1;
+                x -= 1;
+                continue;
+            }
+            if map[x + 1][y + 1] == Space::Air {
+                // the unit of sand attempts to instead move diagonally one step down and to the right.
+                y += 1;
+                x += 1;
+                continue;
+            }
+
+            if map[x][y] == Space::Air {
+                // at rest
+                map[x][y] = Space::Sand;
+                //println!("sand");
+            } else {
+                // out of sand spots
+                //println!("break grains");
+                break 'grains;
+            }
+            //println!("break classique");
+            break;
+        }
+    }
+    let mut count: i32 = 0;
+    for r in map.iter() {
+        for c in r {
+            if *c == Space::Sand {
+                count += 1;
+            }
+        }
+    }
+    println!("{count}");
+    print(&map);
 }
